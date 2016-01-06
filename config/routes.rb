b56521f19412a85163891eaae9f685854c472951
Maxim Matyunin <matyunya@gmail.com>
2016-01-07 02:46:08 +0300

    remove administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
-      resources dashboard_resource
-    end
-    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
-  end
   root to: 'visitors#index'
   devise_for :users
   resources :users
