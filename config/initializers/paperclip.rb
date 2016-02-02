e43ae578e170fc455a95f6b9b90d05bc1232150b
Maxim Matyunin <matyunya@gmail.com>
2016-02-02 15:38:11 +0300

    paperclip international settings

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:url] = 'http://seebe.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
 # config/initializers/paperclip.rb
