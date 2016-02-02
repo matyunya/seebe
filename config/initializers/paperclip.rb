d9412527e06e117b8789b4a24652fd09017efe67
Maxim Matyunin <matyunya@gmail.com>
2016-02-02 15:52:14 +0300

    endpoint

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = 'http://seebe.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+#Paperclip::Attachment.default_options[:url] = 'http://seebe.s3.amazonaws.com'
+#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_host_name] = 'eu-central-1.amazonaws.com'
+#Paperclip::Attachment.default_options[:s3_host_name] = 'eu-central-1.amazonaws.com'
