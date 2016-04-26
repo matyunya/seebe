d2502926ca9ec4a6d0b269a6653b4a550e330b9b
Maxim Matyunin <matyunya@gmail.com>
2016-04-26 21:51:00 +0300

    dotted line for printing

---
+++ public/assets/components/ticket_form-cd2ff63b32c3edb98a584b2e1183b52f3f4e656e4bd9c6936b8be3cb2f51db92.jsx
@@ -0,0 +1,74 @@
+var TicketForm = React.createClass({
+  propTypes: {
+    sections: React.PropTypes.array,
+    tickets: React.PropTypes.array,
+    prices: React.PropTypes.array,
+    row_prices: React.PropTypes.array
+  },
+
+  selected: function() {
+    return typeof this.props.setSelected === 'function' ? this.props.setSelected : {};
+  },
+
+  getInitialState: function() {
+    return {
+      tooltip: false,
+      row: 0,
+      seat: 0,
+      price: 0,
+      taken: false,
+    }
+  },
+
+  showTooltip: function(row, seat, price, taken, e) {
+      this.setState({
+        row: row,
+        seat: seat,
+        price: price,
+        taken: taken,
+        tooltip: true,
+        x: e.clientX,
+        y: e.clientY
+      });
+  },
+
+  hideTooltip: function(e) {
+    this.setState({
+      tooltip: false
+    });
+  },
+
+  render: function() {
+    var style = {
+      textAlign: 'center',
+      lineHeight: '14px',
+      minWidth: '300px',
+      maxWidth: '1100px',
+      fontSize: '11px'
+    }
+    return (
+      <div className="test" style={style}>
+        {this.props.sections.reverse().map(function(section) {
+            return <Section section={section}
+                      key={section.id}
+                      showTooltip={this.showTooltip}
+                      hideTooltip={this.hideTooltip}
+                      tickets={this.props.tickets}
+                      prices={this.props.prices}
+                      row_prices={this.props.row_prices}
+                      setSelected={this.selected()}
+                    />;
+          }.bind(this))}
+        <Tooltip
+          seat={this.state.seat}
+          row={this.state.row}
+          show={this.state.tooltip}
+          price={this.state.price}
+          taken={this.state.taken}
+          x={this.state.x}
+          y={this.state.y}
+        />
+      </div>
+      )
+  }
+});
---
+++ public/assets/components/ticket_form_tooltip-7db01d8f42f4c590c8160a9c79851e30a2083048916f82c0e6a709bfe1f193b8.jsx
@@ -0,0 +1,43 @@
+var Tooltip = React.createClass({
+  propTypes: {
+    show: React.PropTypes.bool
+  },
+
+  getInitialState: function() {
+     return {
+        show: false
+     }
+  },
+
+  text: function() {
+    var text = this.props.price + ' руб.';
+
+    return this.props.taken ? 'Билет продан' : text;
+  },
+
+  sub: function() {
+    var text = this.props.seat + " " + this.props.row;
+    return this.props.taken ? null : text;
+  },
+
+  render: function() {
+    var style = {
+      padding: this.props.show ? '10px' : '0px',
+      fontSize: '16px',
+      position: 'fixed',
+      left: this.props.x - 20,
+      top: this.props.y + 20,
+      lineHeight: '25px',
+      color: 'white',
+      backgroundColor: 'rgba(0,0,0,0.4)',
+      zIndex: 1000
+    };
+
+    return (
+      <span style={style}>
+        {this.props.show ? this.text() : null}<br/>
+        {this.props.show ? this.props.row + " ряд " + this.props.seat + " место" : null }
+      </span>
+      );
+  }
+});
